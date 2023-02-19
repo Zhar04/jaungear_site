55bbdf62979706b156e2a76083baccbaa3353a83
Zhar04 <72005018+Zhar04@users.noreply.github.com>
2023-02-20 02:22:56 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+const App = props => (
+  <LoginForm />
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class LoginForm extends React.Component{
+render(){
+  return(
+    <div id="loginform">
+      <FormHeader title="Login" />
+      <Form />
+      <OtherMethods />
     </div>
-  );
+  )
+}
 }
 
-export default App;
+const FormHeader = props => (
+  <h2 id="headerTitle">{props.title}</h2>
+);
+
+
+const Form = props => (
+ <div>
+   <FormInput description="Username" placeholder="Enter your username" type="text" />
+   <FormInput description="Password" placeholder="Enter your password" type="password"/>
+   <FormButton title="Log in"/>
+ </div>
+);
+
+const FormButton = props => (
+<div id="button" class="row">
+  <button>{props.title}</button>
+</div>
+);
+
+const FormInput = props => (
+<div class="row">
+  <label>{props.description}</label>
+  <input type={props.type} placeholder={props.placeholder}/>
+</div>  
+);
+
+const OtherMethods = props => (
+<div id="alternativeLogin">
+  <label>Or sign in with:</label>
+  <div id="iconGroup">
+    <Facebook />
+    <Twitter />
+    <Google />
+  </div>
+</div>
+);
+
+const Facebook = props => (
+<a href="#" id="facebookIcon"></a>
+);
+
+const Twitter = props => (
+<a href="#" id="twitterIcon"></a>
+);
+
+const Google = props => (
+<a href="#" id="googleIcon"></a>
+);
+
+ReactDOM.render(<App />, document.getElementById('container'));
